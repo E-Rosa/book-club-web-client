5ac4a489ed06e12e43ae25802a79cafb59fe0014
Elias <elias_rrosa@hotmail.com>
2023-05-01 08:48:17 -0300

    endpointURL

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,4 +1,5 @@
-const endpointURL = "https://book-club-web-server.vercel.app"
+const endpointURL = "https://book-club-web-server-git-v101-e-rosa.vercel.app"
+//const endpointURL = "https://book-club-web-client.vercel.app/"
 //const endpointURL = "http://localhost:4000"
 
 export {endpointURL}

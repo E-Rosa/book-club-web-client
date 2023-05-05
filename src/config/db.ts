22562a4903ca36734a6f94e00152d75aafe5cb2a
Elias <elias_rrosa@hotmail.com>
2023-05-05 11:18:55 -0300

    changed endpointURL to official production URL

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,6 @@
 //const endpointURL = "https://book-club-web-server-git-v101-e-rosa.vercel.app"
-//const endpointURL = "https://book-club-web-client.vercel.app/"
+const endpointURL = "https://book-club-web-client.vercel.app"
 //const endpointURL = "http://localhost:4000"
-const endpointURL = "https://book-club-web-server-git-v110-e-rosa.vercel.app"
+//const endpointURL = "https://book-club-web-server-git-v110-e-rosa.vercel.app"
 
 export {endpointURL}

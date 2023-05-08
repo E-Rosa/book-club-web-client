01101a75fe7e2cc06c1ae2f7294c86387615e21e
Elias <elias_rrosa@hotmail.com>
2023-05-08 07:50:35 -0300

    changed to dev env

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,5 @@
-const endpointURL = "https://book-club-web-server.vercel.app"
-//const endpointURL = "http://localhost:4000"
+//const endpointURL = "https://book-club-web-server.vercel.app"
+const endpointURL = "http://localhost:4000"
 //const endpointURL = "https://book-club-web-server-git-v101-e-rosa.vercel.app"
 //const endpointURL = "https://book-club-web-server-git-v110-e-rosa.vercel.app"
 //const endpointURL = "https://book-club-web-server-git-v111-e-rosa.vercel.app"
